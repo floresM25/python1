8d7c73dfc7726500d68c1b6f8e84dd4b0dfedd15
Erik <erik.f.m.0525@gmail.com>
2022-09-13 10:32:17 -0500

    Copia de todo

---
+++ hola.py
@@ -0,0 +1,5 @@
+name =input ("ingresa tu nombre: ")
+age = input(("ingresa tu edad: "))
+print(f"yo soy  {name} y tengo {age} años")
+future = int(age) + 4
+print(f" En el año 2026 tendré {future} años")
