9ed957df486af61a701b42d15a211b24146a6403
Erik <erik.f.m.0525@gmail.com>
2022-09-28 09:42:21 -0500

    Calculadora2

--- calculator.py
+++ calculator.py
@@ -11,9 +11,26 @@ def divide(num1, num2):
     return num1 / num2
 
 if __name__ == '__main__':
-    message = f"Calculadora: \n Elige una opcion\n 1 - suma\n 2 -resta\n 3 - multiplicacion\n 4 - divicion\n 5 - salir\n "
+    message = f"Calculadora: \n Elige una opcion\n 1 - suma\n2 -resta\n3 - multiplicacion\n4 - division\n5 - salir\n "
     while True:
-        opcion = int(input(massage))
+        opcion = int(input(message))
+        #Comparar cada una de las opciones y llamar a la funcion que corresponda
+        if opcion == 1:
+            resultado_suma = suma(23, 54)
+            print("El resultado de la suma es", resultado_suma)
+        elif opcion == 2:
+            resultado_resta = resta(23, 54)
+            print("El resultado de la resta es: ", resultado_resta )
+        elif opcion == 3:
+            resultado_multi = multiply(23, 54)
+            print("El resultado de la multiplicacion es: ", resultado_multi )
+        elif opcion == 4:
+            resultado_div = divide(23, 54)
+            print("El resultado de la division es: ", resultado_div )
+
+
         if opcion == 5:
             print("Bye")
             break 
+        else:
+            print("Opcion Incorrecta")
