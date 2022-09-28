cb8d8d6640596c5d912067c84fc4ba7c5c4521b1
Erik <erik.f.m.0525@gmail.com>
2022-09-28 10:35:31 -0500

    Calculadora funcional

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
+import os
+
 def suma(num1, num2):
     return num1 + num2
 
@@ -8,28 +10,36 @@ def multiply(num1, num2):
     return num1 * num2
 
 def divide(num1, num2):
+    if num2 == 0:
+        num2 = 1
     return num1 / num2
 
+def return_values():
+    num1 = int(input("Ingresa un numero: "))
+    num2 = int(input("Ingresa otro numero: "))
+    return  [num1, num2]
+
+
 if __name__ == '__main__':
-    message = f"Calculadora: \n Elige una opcion\n 1 - suma\n2 -resta\n3 - multiplicacion\n4 - division\n5 - salir\n "
+    message = f"Calculadora: \n Elige una opcion\n1 - suma\n2 - resta\n3 - multiplicacion\n4 - division\n5 - salir\n "
     while True:
         opcion = int(input(message))
         #Comparar cada una de las opciones y llamar a la funcion que corresponda
         if opcion == 1:
-            #Pedir numeros al usuario
-            resultado_suma = suma(23, 54)
+            numeros = return_values()
+            resultado_suma = suma(numeros [0], numeros [1])
             print("El resultado de la suma es", resultado_suma)
         elif opcion == 2:
-              #Pedir numeros al usuario
-            resultado_resta = resta(23, 54)
+            numeros = return_values()
+            resultado_resta = resta(numeros [0], numeros [1])
             print("El resultado de la resta es: ", resultado_resta )
         elif opcion == 3:
-              #Pedir numeros al usuario
-            resultado_multi = multiply(23, 54)
+            numeros = return_values()
+            resultado_multi = multiply(numeros [0], numeros [1])
             print("El resultado de la multiplicacion es: ", resultado_multi )
         elif opcion == 4:
-              #Pedir numeros al usuario
-            resultado_div = divide(23, 54)
+            numeros = return_values()
+            resultado_div = divide(numeros [0], numeros [1])
             print("El resultado de la division es: ", resultado_div )
 
 
@@ -38,3 +48,4 @@ if __name__ == '__main__':
             break 
         else:
             print("Opcion Incorrecta")
+            
